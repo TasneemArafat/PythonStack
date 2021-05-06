d22ddb674cdd456a2677815dfc288a6d2418cc76
Tasneem Arafat <tasneem.arafat@gmail.com>
2021-05-06 14:40:37 +0300

    updating BankAccount assignment

--- _python/python_fundamentals/BankAccount/bank_account.py
+++ _python/python_fundamentals/BankAccount/bank_account.py
@@ -2,38 +2,32 @@ class BankAccount:
     def __init__(self, int_rate=.01, balance=0):
         self.interest = int_rate
         self.account_balance = balance
+        
     def deposit(self, amount):
         self.account_balance += amount
+        return self
     def withdraw(self, amount):
         if(amount>self.account_balance):
             print("Insufficient funds: Charging a $5 fee")
             self.account_balance -= 5
+            return self
         else:
             self.account_balance -= amount
+            return self
     def display_account_info(self):
         print(f"Balance: ${self.account_balance}")
+        return self
     def yield_interest(self):
         if(self.account_balance>0):
             self.account_balance += (self.account_balance*self.interest)
+            return self
 
 firstaccount = BankAccount()
 secondacount = BankAccount()
 
-firstaccount.deposit(30)
-firstaccount.deposit(50)
-firstaccount.deposit(10)
-firstaccount.withdraw(40)
-firstaccount.yield_interest()
-firstaccount.display_account_info()
+firstaccount.deposit(30).deposit(50).deposit(10).withdraw(40).yield_interest().display_account_info()
 
-secondacount.deposit(50)
-secondacount.deposit(50)
-secondacount.withdraw(10)
-secondacount.withdraw(20)
-secondacount.withdraw(30)
-secondacount.withdraw(30)
-secondacount.yield_interest()
-secondacount.display_account_info()
+secondacount.deposit(50).deposit(50).withdraw(10).withdraw(20).withdraw(30).withdraw(30).yield_interest().display_account_info()
 
 
 
