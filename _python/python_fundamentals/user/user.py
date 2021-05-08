15d8bfa7c34c933c16dd05dc4879e09da070ecbf
Tasneem Arafat <tasneem.arafat@gmail.com>
2021-05-08 12:37:48 +0300

    adding users with bank accounts assignment

--- _python/python_fundamentals/user/user.py
+++ _python/python_fundamentals/user/user.py
@@ -1,34 +1,39 @@
 from bank_account import BankAccount
 class User:
-    def __init__(self, name, email):
+    def __init__(self, name, email,number=1):
         self.name = name
         self.email = email
-        self.account = BankAccount()
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
+        self.accountsnumber = number
+        self.bank_accounts = []
+        for i in range(number):
+            self.bank_accounts.append(BankAccount())
+    def make_deposit(self, amount,i):
+        self.bank_accounts[i-1].deposit(amount)
         return self
-    def make_withdrawal(self, amount):
-        self.account.withdraw(amount)
+    def make_withdrawal(self, amount,i):
+        self.bank_accounts[i-1].withdraw(amount)
         return self
     def display_user_balance(self):
-        print(f"User: {self.name}, Balance: {self.account.account_balance}")
+        print(f"User: {self.name}")
+        for i in range(len(self.bank_accounts)):
+            self.bank_accounts[i].display_account_info()
         return self
-    def transfer(self,other_user,amount):
-        self.make_withdrawal(amount)
-        other_user.make_deposit(amount)
+    def transfer(self,other_user,amount,selfaccountnum,otheraccountnum):
+        self.make_withdrawal(amount,selfaccountnum)
+        other_user.make_deposit(amount,otheraccountnum)
         return self
 
-tasneem = User("tasneem","Tasneem@gmail.com")
-amro = User("Amro","Amro@gmail.com")
+tasneem = User("tasneem","Tasneem@gmail.com",2)
+amro = User("Amro","Amro@gmail.com",1)
 hadeel = User("Hadeel","Hadeel@gmail.com")
 
-tasneem.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(200).display_user_balance()
+tasneem.make_deposit(100,1).make_deposit(200,2).make_deposit(300,1).make_withdrawal(200,2).display_user_balance()
 
-amro.make_deposit(500).make_deposit(300).make_withdrawal(700).make_withdrawal(50).display_user_balance()
+amro.make_deposit(500,1).make_deposit(300,1).make_withdrawal(700,1).make_withdrawal(50,1).display_user_balance()
 
-hadeel.make_deposit(1000).make_withdrawal(200).make_withdrawal(500).make_withdrawal(200).display_user_balance()
+hadeel.make_deposit(1000,1).make_withdrawal(200,1).make_withdrawal(500,1).make_withdrawal(200,1).display_user_balance()
 
-tasneem.transfer(hadeel,100).display_user_balance()
+tasneem.transfer(hadeel,100,2,1).display_user_balance()
 hadeel.display_user_balance()
 
 
